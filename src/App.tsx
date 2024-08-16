32d8593622250e497429898fd7725ab99e6d5eea
Nalin Singh <nalinsingh20@gmail.com>
2024-08-17 00:39:03 +0530

    Add : Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/system/toaster";
 
 export const Application = () => {
   return (
     <>
+      {/* Toaster Component will be used to display the notifications, documentation https://ui.shadcn.com/docs/components/toast */}
+      <Toaster />
       {/* Header, Navigation, Footer like Global Components that will be used in all pages can be put here, and Outlet will be used to render the children components based on the route and the router definition provided in the main.tsx file*/}
       <Outlet />
     </>
